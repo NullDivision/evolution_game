90d2d88c79e66137583d9ac9772b24ab1ebef021
NullDivison <penumbra.scilicet@gmail.com>
2023-10-10 18:26:57 +0300

    feat: add entity velocity

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ use rand::Rng;
 #[derive(Component)]
 struct Score(i32);
 
-// Character component
+// Velocity component
 #[derive(Component)]
-struct Character;
+struct Velocity {
+    x: f32,
+    y: f32,
+    x_max: f32,
+    y_max: f32,
+}
 
 fn game_startup(
     mut commands: Commands,
@@ -19,11 +24,16 @@ fn game_startup(
     // Player
     commands.spawn((
         bevy::sprite::MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            mesh: meshes.add(shape::Circle::new(12.).into()).into(),
             material: materials.add(Color::RED.into()),
             ..default()
         },
-        Character,
+        Velocity {
+            x: 0.,
+            y: 0.,
+            x_max: 1000.,
+            y_max: 1000.,
+        },
     ));
 
     // Other characters
@@ -41,7 +51,12 @@ fn game_startup(
                 )),
                 ..default()
             },
-            Character,
+            Velocity {
+                x: 0.,
+                y: 0.,
+                x_max: 1000.,
+                y_max: 1000.,
+            },
         ));
     }
 }
@@ -68,18 +83,45 @@ fn score_board_startup(mut commands: Commands) {
     ));
 }
 
+const DIRECTION_CHANGE_WEIGHT: f64 = 0.1;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (game_startup, score_board_startup))
         .add_systems(
             FixedUpdate,
-            |mut character_transforms: Query<&mut Transform, With<Character>>| {
+            |mut character_transforms: Query<(&mut Transform, &mut Velocity)>,
+             time_step: Res<FixedTime>| {
                 let mut rng = rand::thread_rng();
 
-                for mut transform in character_transforms.iter_mut() {
+                for (mut transform, mut velocity) in character_transforms.iter_mut() {
+                    let x_velocity_offset = rng.gen_range(-velocity.x_max..=velocity.x_max);
+                    let y_velocity_offset = rng.gen_range(-velocity.x_max..=velocity.y_max);
+
+                    // Use weighted offset to determine direction
+                    if rng.gen_bool(DIRECTION_CHANGE_WEIGHT) {
+                        velocity.x = -x_velocity_offset;
+                    } else {
+                        velocity.x = x_velocity_offset;
+                    }
+                    if rng.gen_bool(DIRECTION_CHANGE_WEIGHT) {
+                        velocity.y = -y_velocity_offset;
+                    } else {
+                        velocity.y = y_velocity_offset;
+                    }
+
+                    // Ensure velocity is within bounds while maintaining direction
+                    if velocity.x.abs() >= velocity.x_max {
+                        velocity.x = velocity.x_max * velocity.x.signum();
+                    }
+                    if velocity.y.abs() >= velocity.y_max {
+                        velocity.y = velocity.y_max * velocity.y.signum();
+                    }
+
                     // Move character around randomly
-                    transform.translation.x = rng.gen_range(-1.0..1.0);
+                    transform.translation.x += velocity.x * time_step.period.as_secs_f32();
+                    transform.translation.y += velocity.y * time_step.period.as_secs_f32();
                 }
             },
         )
