e97708ebd6b54d21b767fb9dba7845fee7d0ac24
NullDivison <penumbra.scilicet@gmail.com>
2023-10-10 14:22:54 +0300

    feat: add entity jitter

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,86 @@
-use bevy::{
-    prelude::{default, Camera2dBundle, Commands, Component, Query},
-    text::{Text, Text2dBundle, TextSection, TextStyle},
-};
+use bevy::prelude::*;
+use rand::Rng;
 
 // Score component
 #[derive(Component)]
 struct Score(i32);
 
+// Character component
+#[derive(Component)]
+struct Character;
+
+fn game_startup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    // Player
+    commands.spawn((
+        bevy::sprite::MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(Color::RED.into()),
+            ..default()
+        },
+        Character,
+    ));
+
+    // Other characters
+    for i in 0..10 {
+        let enemy_radius = 10.;
+
+        commands.spawn((
+            bevy::sprite::MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(enemy_radius).into()).into(),
+                material: materials.add(Color::WHITE.into()),
+                transform: Transform::from_translation(Vec3::new(
+                    i as f32 * (enemy_radius + (enemy_radius / 2.)),
+                    0.,
+                    0.,
+                )),
+                ..default()
+            },
+            Character,
+        ));
+    }
+}
+
+fn score_board_startup(mut commands: Commands) {
+    commands.spawn((
+        TextBundle {
+            text: Text {
+                sections: vec![TextSection::new(
+                    format!("Score: {}", 0),
+                    TextStyle { ..default() },
+                )],
+                ..default()
+            },
+            style: Style {
+                position_type: PositionType::Absolute,
+                right: Val::Px(5.),
+                top: Val::Px(5.),
+                ..default()
+            },
+            ..default()
+        },
+        Score(0),
+    ));
+}
+
 fn main() {
-    bevy::app::App::new()
-        .add_plugins(bevy::DefaultPlugins)
-        .add_systems(bevy::app::Startup, |mut commands: Commands| {
-            commands.spawn(Camera2dBundle::default());
-            commands.spawn((
-                Text2dBundle {
-                    text: Text {
-                        sections: vec![TextSection::new(
-                            format!("Score: {}", 0),
-                            TextStyle { ..default() },
-                        )],
-                        ..default()
-                    },
-                    ..default()
-                },
-                Score(0),
-            ));
-        })
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (game_startup, score_board_startup))
         .add_systems(
-            bevy::app::FixedUpdate,
-            |mut score: Query<&mut Score>, mut text: Query<&mut Text>| {
-                score.single_mut().0 += 1;
-                text.single_mut().sections[0].value = format!("Score: {}", score.single().0);
+            FixedUpdate,
+            |mut character_transforms: Query<&mut Transform, With<Character>>| {
+                let mut rng = rand::thread_rng();
+
+                for mut transform in character_transforms.iter_mut() {
+                    // Move character around randomly
+                    transform.translation.x = rng.gen_range(-1.0..1.0);
+                }
             },
         )
         .run();
